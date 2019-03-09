2e8b6376a2a07df8dc8fe297d5ec33856a096170
Grant Walker <gwwalker91@gmail.com>
2019-03-08 20:23:21 -0800

    Added the initial setup for a base asp.net core site Added the initial setup for having cypress UI test in your asp.net core site

---
+++ BaseAspNetCoreSite/BaseAspNetCoreSite/cypress/integration/UITests/SimpleFirstTest.spec.js
@@ -0,0 +1,27 @@
+﻿/// <reference types="Cypress" />
+
+context('Window', () => {
+  beforeEach(() => {
+    cy.visit('https://localhost:5001/testviews/');
+  })
+
+  it('cy.window() - get the global window object', () => {
+    // https://on.cypress.io/window
+    cy.window().should('have.property', 'top');
+  })
+
+  it('cy.document() - get the document object', () => {
+    // https://on.cypress.io/document
+    cy.document().should('have.property', 'charset').and('eq', 'UTF-8');
+  })
+
+  it('cy.title() - get the title', () => {
+    // https://on.cypress.io/title
+    cy.title().should('include', 'Index');
+  })
+
+  it('cy.find - Find text on the screen', () => {
+    // https://on.cypress.io/title
+    cy.get("[data-test-selector='objecttofind']").contains("We found it!");
+  })
+})
